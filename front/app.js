050fa1504cbd64f23b511a345ef5670c56261814
jblavisse <jean-baptiste@jblavisse.fr>
2021-07-22 12:24:18 +0200

    It's the end of the world as we know it

--- front/app.js
+++ front/app.js
@@ -4,9 +4,33 @@ socket.on("connect", () => {
 
     let btn = document.getElementById('poupee');
 
+    let green = document.getElementById('green');
+    let blue = document.getElementById('blue');
+    let red = document.getElementById('red');
+
+    green.addEventListener("click", () => {
+        // 1. Evenement color envoyé
+        socket.emit("color", "green");
+    });
+
+    blue.addEventListener("click", () => {
+        // 1. Evenement color envoyé
+        socket.emit("color", "blue");
+    });
+
+    red.addEventListener("click", () => {
+        // 1. Evenement color envoyé
+        socket.emit("color", "red");
+    });
+
     btn.addEventListener("click", () => {
         console.log("Coucou Krystel!");
         // Envoyer un événement au serveur
         socket.emit("doll", "Un petit truc");
+    });
+
+    // 4. Réception colorReceived
+    socket.on('colorReceived', (color) => {
+        document.body.style.backgroundColor=color;
     });
 });
--- server.js
+++ server.js
@@ -18,6 +18,21 @@ io.on('connection', (socket) => {
         console.log(msg);
     });
 
+    // 2. Evenement color reçu
+    socket.on('color', (color) => {
+
+        console.log(color);
+        // Ca transmet l'événement au front qui m'a contacté
+        // Mais qu'à lui
+
+        // 3. EMission colorReceived
+        socket.emit('colorReceived', color);
+
+        // Ca transmet l'événément à tous sauf au front
+        // qui m'a contacté
+        socket.broadcast.emit('colorReceived', color);
+    });
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
         });
